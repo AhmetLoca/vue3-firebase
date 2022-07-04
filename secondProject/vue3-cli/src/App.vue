5c94db1d0dd87f4d22b29dbe4a375f0c31dc3999
ahmetloca <locaahmet@gmail.com>
2022-07-04 18:18:11 +0300

    vue3 kurulum ve ref ile inputa ulasma

---
+++ secondProject/vue3-cli/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+ <div>
+  <h3>{{title}}</h3>
+  <input type="text" ref="isim"/>
+  <input type="text" ref="parola"/>
+  <button @click="handleClick">Tıkla</button>
+  <br>
+  {{sonuc}}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      title:'Hi Vue CLI',
+      sonuc:''
+    }
+  },
+  methods:{
+    handleClick(){
+      console.log(this.$refs.isim.value)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
