1c9c9eb4e27ff23c28a9ad50da7721882c076086
ahmetloca <locaahmet@gmail.com>
2022-07-04 17:16:05 +0300

    mini uygulama, computed kullanımı

---
+++ firstProject/app.js
@@ -0,0 +1,49 @@
+const app = Vue.createApp({
+  template: `
+    <h2>İçecek Seçiniz</h2>
+      <ul>
+        <li v-for="icecek in icecekler"
+          class="li"
+          :class="{secilen:icecek.sec}"
+          @click="icecekSec(icecek)">
+         {{icecek.isim}}
+        </li>
+      </ul>
+      
+      <button @click="yazdir()">Secilen içecekleri yazdir</button>
+
+      <hr>
+
+      <h3>Secilen İcecekler</h3>
+      <ul>
+        <li v-for="secilen in secilenIcecekler" class="green">
+          {{secilen.isim}}
+        </li>
+      </ul>
+  `,
+
+  data() {
+    return {
+      icecekler: [
+        { isim: "Su", sec: false },
+        { isim: "Ayran", sec: false }
+      ]
+    }
+  },
+  methods: {
+    icecekSec(icecek) {
+      icecek.sec = !icecek.sec;
+    },
+    yazdir() {
+      const secilenler = this.icecekler.filter(i => i.sec == true)
+      console.log(secilenler);
+    }
+  },
+  computed: {
+    secilenIcecekler() {
+      return secilenler = this.icecekler.filter(i => i.sec)
+    }
+  }
+});
+
+app.mount('#app');
